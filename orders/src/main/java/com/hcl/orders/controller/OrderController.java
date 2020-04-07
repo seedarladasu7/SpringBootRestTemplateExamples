3e7ae1be88b9c02f173b74387ce22c5bc29a40ab
seedarladasu7 <s.mariyadasu@gmail.com>
2020-04-07 11:12:35 +0530

    Initial Commit

---
+++ orders/src/main/java/com/hcl/orders/controller/OrderController.java
@@ -0,0 +1,111 @@
+package com.hcl.orders.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.orders.dto.Order;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+	
+	static String sysoOrderCtrl = "from order controller ===============>>";
+
+	@GetMapping("")
+	public List<Order> getAll() {
+
+		System.out.println(sysoOrderCtrl);
+
+		List<Order> orders = new ArrayList<>();
+
+		Order order = new Order();
+		order.setId(101);
+		order.setDes("desc1");
+		orders.add(order);
+
+		order = new Order();
+		order.setId(102);
+		order.setDes("desc2");
+		orders.add(order);
+
+		return orders;
+	}
+
+	@GetMapping("/{userId}")
+	public List<Order> getAllById(@PathVariable String userId) {
+
+		System.out.println(sysoOrderCtrl + userId);
+
+		List<Order> orders = new ArrayList<>();
+
+		Order order = new Order();
+		order.setId(103);
+		order.setDes("desc3");
+		orders.add(order);
+
+		order = new Order();
+		order.setId(104);
+		order.setDes("desc4");
+		orders.add(order);
+
+		return orders;
+	}
+
+	@GetMapping("/byReqParam")
+	public List<Order> getAllByReqParam(@RequestParam("userId") String userId) {
+
+		System.out.println(sysoOrderCtrl + userId);
+
+		List<Order> orders = new ArrayList<>();
+
+		Order order = new Order();
+		order.setId(105);
+		order.setDes("desc5");
+		orders.add(order);
+
+		order = new Order();
+		order.setId(106);
+		order.setDes("desc6");
+		orders.add(order);
+
+		return orders;
+	}
+
+	@PostMapping("/byPostReqParam")
+	public List<Order> getAllByPostReqParam() {
+
+		System.out.println(sysoOrderCtrl);
+
+		List<Order> orders = new ArrayList<>();
+
+		Order order = new Order();
+		order.setId(107);
+		order.setDes("desc7");
+		orders.add(order);
+
+		order = new Order();
+		order.setId(108);
+		order.setDes("desc8");
+		orders.add(order);
+
+		return orders;
+	}
+
+	@PostMapping("/byBody")
+	public Order getAllByPostReqBody(@RequestBody Order order) {
+
+		System.out.println(sysoOrderCtrl);
+
+		order.setDes(order.getDes() + " updated");
+
+		return order;
+	}
+}
---
+++ user/src/main/java/com/hcl/user/controller/UserController.java
@@ -0,0 +1,142 @@
+package com.hcl.user.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	RestTemplate restTemplate = RestTemplateSingleton.getInstance();
+
+	@GetMapping("") 
+	public String getUserOrders() {
+
+		final String uri = "http://localhost:8081/demo/orders";
+
+		String result = restTemplate.getForObject(uri, String.class);
+
+		return result;
+
+	}
+
+	@GetMapping("/{userId}")
+	public String getUserOrdersById(@PathVariable String userId) {
+
+		final String uri = "http://localhost:8081/demo/orders/" + userId;
+
+		String result = restTemplate.getForObject(uri, String.class);
+
+		return result;
+
+	}
+
+	@GetMapping("/byReqParam")
+	public String getUserOrdersByReqParam(@RequestParam("userId") String userId) {
+
+		final String uri = "http://localhost:8081/demo/orders/byReqParam";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("userId", userId);
+
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri);
+		for (Map.Entry<String, String> entry : params.entrySet()) {
+			builder.queryParam(entry.getKey(), entry.getValue());
+		}
+
+		String result = restTemplate.getForObject(builder.toUriString(), String.class);
+
+		return result;
+
+	}
+
+	@GetMapping("/postParam")
+	public ResponseEntity<String> testPostWithParam(@RequestParam String userId) {
+
+		String uri = "http://localhost:8081/demo/orders/byPostReqParam";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+		map.add("userId", userId);
+
+		HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(map, headers);
+
+		ResponseEntity<String> response = restTemplate.postForEntity(uri, httpEntity, String.class);
+
+		System.out.println(response);
+		return response;
+	}
+
+	@GetMapping("/byBody")
+	public ResponseEntity<String> testReqBody() throws JSONException {
+
+		String uri = "http://localhost:8081/demo/orders/byBody";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		JSONObject request = new JSONObject();
+		request.put("id", 110);
+		request.put("des", "test10");
+
+		HttpEntity<String> entity = new HttpEntity<String>(request.toString(), headers);
+
+		ResponseEntity<String> response = restTemplate.postForEntity(uri, entity, String.class);
+
+		System.out.println(response);
+		return response;
+
+	}
+
+	@GetMapping("/byBody2")
+	public ResponseEntity<String> testReqBody2() throws JSONException {
+
+		String uri = "http://localhost:8081/demo/orders/byBody";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		JSONObject request = new JSONObject();
+		request.put("id", 1122);
+		request.put("des", "usk1");
+
+		HttpEntity<String> entity = new HttpEntity<String>(request.toString(), headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
+		return response;
+
+	}
+
+	public static class RestTemplateSingleton  {    
+	    private static class RestTemplateSingletonHolder {    
+	        public static final RestTemplate instance = new RestTemplate();
+	    }    
+
+	    public static RestTemplate getInstance() {    
+	        return RestTemplateSingletonHolder.instance;    
+	    }    
+	}
+
+}
